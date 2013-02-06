fe25c57726b469d450ae1e24da3268d11dcbc0e8
Ronald Martin <ronaldmartin@gmail.com>
2013-02-06 12:32:38 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChoresAt4030::Application.config.secret_token = '713957d1f492a8ef8a6e6c1dea798c8f35cfe5ce8ec313dc5d11ec53b48464b20d2faa282f78133a99ec39d47db40ecab652962babbb9f09bbb2fb6474a70861'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ChoresAt4030::Application.config.session_store :cookie_store, key: '_chores-at-4030_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ChoresAt4030::Application.config.session_store :active_record_store
