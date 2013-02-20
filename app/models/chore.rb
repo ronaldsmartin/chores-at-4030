13bd2c5fae2bdf026ffcaa03539f644ac3fa1f01
Ronald Martin <ronaldmartin@gmail.com>
2013-02-20 13:55:30 -0500

    Added presence and format validations to Chores

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,6 @@
 class Chore < ActiveRecord::Base
+	attr_accessible :chore, :assignedto, :chorefinished, :dateadded, :datefinished
+
+	validates :chore, :assignedto, presence: true
+	validates :assignedto, :format => { :with => /\A[a-zA-Z]+\z/, :message => "Only letters allowed" }
 end
