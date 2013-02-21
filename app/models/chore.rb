d8f117580819fbe1fb204929245ce16a4e69575f
Ronald Martin <ronaldmartin@gmail.com>
2013-02-20 19:28:09 -0500

    Added rspec to development & test

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,6 +1,6 @@
 class Chore < ActiveRecord::Base
 	attr_accessible :chore, :assignedto, :chorefinished, :dateadded, :datefinished
 
-	validates :chore, :assignedto, presence: true
+	validates :chore, :assignedto, presence: { :message => "Field must be nonempty"}
 	validates :assignedto, :format => { :with => /\A[a-zA-Z]+\z/, :message => "Only letters allowed" }
 end
